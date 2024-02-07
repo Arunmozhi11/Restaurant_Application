75219b3d4e8c94c11ba9ae29b9f0bfcef147815e
Arunmozhi11 <159102619+Arunmozhi11@users.noreply.github.com>
2024-02-07 11:04:42 +0530

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -71,7 +71,7 @@ const Menu = () => {
 
   return (
     <>
-      <div className="w-screen min-h-screen flex items-center justify-center flex-col bg-primary">
+      <div className="w-screen min-h-screen flex items-center justify-center flex-col bg-violet-50">
         <Header />
 
         <div className="w-full flex flex-col  justify-between mt-40 px-6 md:px-24 2xl:px-96 gap-12 pb-24">
